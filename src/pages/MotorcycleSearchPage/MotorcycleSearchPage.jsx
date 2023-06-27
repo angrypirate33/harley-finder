e7e5423fb2e9519b889c1e80be3f684a4f37e31f
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-27 08:28:15 -0400

    Formats search results as cards.

--- src/pages/MotorcycleSearchPage/MotorcycleSearchPage.jsx
+++ src/pages/MotorcycleSearchPage/MotorcycleSearchPage.jsx
@@ -23,11 +23,24 @@ export default function MotorcycleSearchPage() {
     return (
         <div>
             <h2>MotorcycleSearchPage</h2>
-            <ul>
+            <div className="row">
                 {motorcycles.map(motorcycle => (
-                    <li className='white-text' key={motorcycle._id}>{motorcycle.year} {motorcycle.model}</li>
+                    <div className="col s12 m6 l4" key={motorcycle._id}>
+                        <div className="card orange">
+                            <div className="card-content">
+                                <span className="card-title black-text">{motorcycle.year} {motorcycle.model}</span>
+                                <ul>
+                                <li className='black-text'>Type: {motorcycle.type}</li>
+                                <li className='black-text'>Displacement: {motorcycle.displacement}</li>
+                                <li className='black-text'>Power: {motorcycle.power}</li>
+                                <li className='black-text'>Gearbox: {motorcycle.gearbox}</li>
+                                <li className='black-text'>Seat Height: {motorcycle.seat_height}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
