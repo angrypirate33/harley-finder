8f9d5d3db38428bfc64547532709ebdf6bdeab92
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-27 16:02:13 -0400

    Adds formatting to NewWishlistForm.

--- src/components/NewWishlistForm/NewWishlistForm.jsx
+++ src/components/NewWishlistForm/NewWishlistForm.jsx
@@ -13,44 +13,52 @@ export default function NewWishlistForm() {
 
     return (
         <div className='row'>
-            <form className='col s12' onSubmit={handleSubmit}>
-                <div className='row'>
-                    <div className='input-field col s12'>
-                        <input
-                            type='text'
-                            id='name'
-                            value={name}
-                            autoComplete='off'
-                            onChange={(evt) => setName(evt.target.value)}
-                            required
-                        />
-                        <label htmlFor="name">Name</label>
+            <div className='col s12 m8 offset-m2 l6 offset-l3'>
+                <div className='card orange'>
+                    <div className='card-content'>
+                        <span className='card-title black-text'>Create New Wishlist</span>
+                        <form onSubmit={handleSubmit}>
+                            <div className='row'>
+                                <div className='input-field col s12'>
+                                    <input
+                                        type='text'
+                                        id='name'
+                                        value={name}
+                                        onChange={(evt) => setName(evt.target.value)}
+                                        required
+                                        className='black-text'
+                                        autoComplete='off'
+                                    />
+                                    <label htmlFor='name' className='white-text'>Name</label>
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <div className='input-field col s12'>
+                                    <textarea
+                                        id='description'
+                                        className='materialize-textarea'
+                                        value={description}
+                                        onChange={(evt) => setDescription(evt.target.value)}
+                                        autoComplete='off'
+                                    ></textarea>
+                                    <label htmlFor='description' className='white-text'>Description</label>
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <div className='col s12'>
+                                    <button
+                                        className='btn black waves-effect waves-light'
+                                        type='submit'
+                                        name='action'
+                                    >
+                                        Create
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div className='row'>
-                    <div className='input-field col s12'>
-                        <textarea 
-                            id='description'
-                            className='materialize-textarea'
-                            value={description}
-                            autoComplete='off'
-                            onChange={(evt) => setDescription(evt.target.value)}
-                        ></textarea>
-                        <label htmlFor='description'>Description</label>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col s12'>
-                        <button
-                            className='btn waves-effect waves-light'
-                            type='submit'
-                            name='action'
-                        >
-                            Create
-                        </button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     )
 }
