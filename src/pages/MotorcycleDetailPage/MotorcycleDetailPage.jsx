cfb1fdf3055a641aaf611b5a5728602f2d2d192c
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-06-27 11:38:09 -0400

    Modifies MotorcycleDetailPage to only render a detail line if that field is not null.

--- src/pages/MotorcycleDetailPage/MotorcycleDetailPage.jsx
+++ src/pages/MotorcycleDetailPage/MotorcycleDetailPage.jsx
@@ -31,44 +31,44 @@ export default function MotorcycleDetailPage() {
             <div className="card-content black-text">
                 <span className="card-title">{motorcycle.year} {motorcycle.model}</span>
                 <ul>
-                    <li>Type: {motorcycle.type}</li>
-                    <li>Displacement: {motorcycle.displacement}</li>
-                    <li>Engine: {motorcycle.engine}</li>
-                    <li>Power: {motorcycle.power}</li>
-                    <li>Torque: {motorcycle.torque}</li>
-                    <li>Top Speed: {motorcycle.top_speed}</li>
-                    <li>Gearbox: {motorcycle.gearbox}</li>
-                    <li>Transmission: {motorcycle.transmission}</li>
-                    <li>Fuel Capacity: {motorcycle.fuel_capacity}</li>
-                    <li>Fuel Consumption: {motorcycle.fuel_consumption}</li>
-                    <li>Compression Ratio: {motorcycle.compression}</li>
-                    <li>Bore Stroke: {motorcycle.bore_stroke}</li>
-                    <li>Valves Per Cylinder: {motorcycle.valves_per_cylinder}</li>
-                    <li>Fuel System: {motorcycle.fuel_system}</li>
-                    <li>Fuel Control: {motorcycle.fuel_control}</li>
-                    <li>Ignition: {motorcycle.ignition}</li>
-                    <li>Lubrication: {motorcycle.lubrication}</li>
-                    <li>Cooling: {motorcycle.cooling}</li>
-                    <li>Clutch: {motorcycle.clutch}</li>
-                    <li>Emissions: {motorcycle.emission}</li>
-                    <li>Frame: {motorcycle.frame}</li>
-                    <li>Front Suspension: {motorcycle.front_suspension}</li>
-                    <li>Front Wheel Travel: {motorcycle.front_wheel_travel}</li>
-                    <li>Rear Suspension: {motorcycle.rear_suspension}</li>
-                    <li>Rear Wheel Travel: {motorcycle.rear_wheel_travel}</li>
-                    <li>Front Tire: {motorcycle.front_tire}</li>
-                    <li>Rear Tire: {motorcycle.rear_tire}</li>
-                    <li>Front Brakes: {motorcycle.front_brakes}</li>
-                    <li>Rear Brakes: {motorcycle.rear_brakes}</li>
-                    <li>Dry Weight: {motorcycle.dry_weight}</li>
-                    <li>Total Weight: {motorcycle.total_weight}</li>
-                    <li>Seat Height: {motorcycle.seat_height}</li>
-                    <li>Total Height: {motorcycle.total_height}</li>
-                    <li>Total Length: {motorcycle.total_length}</li>
-                    <li>Total Width: {motorcycle.total_width}</li>
-                    <li>Ground Clearance: {motorcycle.ground_clearance}</li>
-                    <li>Wheelbase: {motorcycle.wheelbase}</li>
-                    <li>Starter: {motorcycle.starter}</li>
+                    {motorcycle.type && <li>Type: {motorcycle.type}</li>}
+                    {motorcycle.displacement && <li>Displacement: {motorcycle.displacement}</li>}
+                    {motorcycle.engine && <li>Engine: {motorcycle.engine}</li>}
+                    {motorcycle.power && <li>Power: {motorcycle.power}</li>}
+                    {motorcycle.torque && <li>Torque: {motorcycle.torque}</li>}
+                    {motorcycle.top_speed && <li>Top Speed: {motorcycle.top_speed}</li>}
+                    {motorcycle.gearbox && <li>Gearbox: {motorcycle.gearbox}</li>}
+                    {motorcycle.transmission && <li>Transmission: {motorcycle.transmission}</li>}
+                    {motorcycle.fuel_capacity && <li>Fuel Capacity: {motorcycle.fuel_capacity}</li>}
+                    {motorcycle.fuel_consumption && <li>Fuel Consumption: {motorcycle.fuel_consumption}</li>}
+                    {motorcycle.seat_height && <li>Seat Height: {motorcycle.seat_height}</li>}
+                    {motorcycle.compression && <li>Compression Ratio: {motorcycle.compression}</li>}
+                    {motorcycle.bore_stroke && <li>Bore Stroke: {motorcycle.bore_stroke}</li>}
+                    {motorcycle.valves_per_cylinder && <li>Valves Per Cylinder: {motorcycle.valves_per_cylinder}</li>}
+                    {motorcycle.fuel_system && <li>Fuel System: {motorcycle.fuel_system}</li>}
+                    {motorcycle.fuel_control && <li>Fuel Control: {motorcycle.fuel_control}</li>}
+                    {motorcycle.ignition && <li>Ignition: {motorcycle.ignition}</li>}
+                    {motorcycle.lubrication && <li>Lubrication: {motorcycle.lubrication}</li>}
+                    {motorcycle.cooling && <li>Cooling: {motorcycle.cooling}</li>}
+                    {motorcycle.clutch && <li>Clutch: {motorcycle.clutch}</li>}
+                    {motorcycle.emission && <li>Emissions: {motorcycle.emission}</li>}
+                    {motorcycle.frame && <li>Frame: {motorcycle.frame}</li>}
+                    {motorcycle.front_suspension && <li>Front Suspension: {motorcycle.front_suspension}</li>}
+                    {motorcycle.front_wheel_travel && <li>Front Wheel Travel: {motorcycle.front_wheel_travel}</li>}
+                    {motorcycle.rear_suspension && <li>Rear Suspension: {motorcycle.rear_suspension}</li>}
+                    {motorcycle.rear_wheel_travel && <li>Rear Wheel Travel: {motorcycle.rear_wheel_travel}</li>}
+                    {motorcycle.front_tire && <li>Front Tire: {motorcycle.front_tire}</li>}
+                    {motorcycle.rear_tire && <li>Rear Tire: {motorcycle.rear_tire}</li>}
+                    {motorcycle.front_brakes && <li>Front Brakes: {motorcycle.front_brakes}</li>}
+                    {motorcycle.rear_brakes && <li>Rear Brakes: {motorcycle.rear_brakes}</li>}
+                    {motorcycle.dry_weight && <li>Dry Weight: {motorcycle.dry_weight}</li>}
+                    {motorcycle.total_weight && <li>Total Weight: {motorcycle.total_weight}</li>}
+                    {motorcycle.total_height && <li>Total Height: {motorcycle.total_height}</li>}
+                    {motorcycle.total_length && <li>Total Length: {motorcycle.total_length}</li>}
+                    {motorcycle.total_width && <li>Total Width: {motorcycle.total_width}</li>}
+                    {motorcycle.ground_clearance && <li>Ground Clearance: {motorcycle.ground_clearance}</li>}
+                    {motorcycle.wheelbase && <li>Wheelbase: {motorcycle.wheelbase}</li>}
+                    {motorcycle.starter && <li>Starter: {motorcycle.starter}</li>}
                 </ul>
             </div>
         </div>
