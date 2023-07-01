0be001143db64e3951df694d0423d455a406d615
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-01 15:49:46 -0400

    Adds first steps to mobile responsive nav. Adds hamburger menu with nav links, but links aren't clickable for some reason.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import * as userService from '../../utilities/users-service'
 import './Nav.css'
@@ -8,18 +9,33 @@ export default function Nav({ user, setUser }) {
         setUser(null)
     }
 
+    useEffect(() => {
+        let elems = document.querySelectorAll('.sidenav');
+        let instances = window.M.Sidenav.init(elems, {});
+    }, []);
+
     return (
         <div className='navbar-fixed'>
             <nav className='orange'>
                 <div className='nav-wrapper container'>
                     <span className='black-text left' id='welcomeMessage' >Welcome, <span id='userName'>{user.name}</span>!</span>
-                    <Link to='/' className='black-text waves-effect waves-light navLink'>Home</Link>
-                    <Link to='/search' className='black-text waves-effect waves-light navLink'>Search Harleys</Link>
-                    <Link to='/wishlists' className='black-text waves-effect waves-light navLink'>Your Wishlists</Link>
-                    <Link to='/about' className='black-text waves-effect waves-light navLink'>About</Link>
-                    &nbsp;&nbsp;<Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light right' id='logoutLink'>Log Out</Link>
+                    <a href="#" data-target="mobile-links" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
+                    <ul className='right hide-on-med-and-down nav-links'>
+                        <li><Link to='/' className='black-text waves-effect waves-light'>Home</Link></li>
+                        <li><Link to='/search' className='black-text waves-effect waves-light'>Search Harleys</Link></li>
+                        <li><Link to='/wishlists' className='black-text waves-effect waves-light'>Your Wishlists</Link></li>
+                        <li><Link to='/about' className='black-text waves-effect waves-light'>About</Link></li>
+                        <li><Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light'>Log Out</Link></li>
+                    </ul>
                 </div>
             </nav>
+            <ul className="sidenav" id="mobile-links">
+                <li><Link to='/' className='black-text waves-effect waves-light'>Home</Link></li>
+                <li><Link to='/search' className='black-text waves-effect waves-light'>Search Harleys</Link></li>
+                <li><Link to='/wishlists' className='black-text waves-effect waves-light'>Your Wishlists</Link></li>
+                <li><Link to='/about' className='black-text waves-effect waves-light'>About</Link></li>
+                <li><Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light'>Log Out</Link></li>
+            </ul>
         </div>
     )
 }
