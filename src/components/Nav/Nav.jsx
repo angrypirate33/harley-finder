cb8705f0cbef97418b5c15fc318df4ac0c1642a4
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-01 14:34:45 -0400

    Fixes navbar to be slightly responsive on smaller screens.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,14 +9,16 @@ export default function Nav({ user, setUser }) {
     }
 
     return (
-        <nav className='navbar-fixed orange'>
-            <div className='nav-wrapper'>
-                <span className='black-text left' id='welcomeMessage' >Welcome, <span id='userName'>{user.name}</span>!</span>
-                <Link to='/search' className='black-text waves-effect waves-light navLink'>Search Harleys</Link>
-                <Link to='/wishlists' className='black-text waves-effect waves-light navLink'>Your Wishlists</Link>
-                <Link to='/about' className='black-text waves-effect waves-light navLink'>About</Link>
-                &nbsp;&nbsp;<Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light right' id='logoutLink'>Log Out</Link>
-            </div>
-        </nav>
+        <div className='navbar-fixed'>
+            <nav className='orange'>
+                <div className='nav-wrapper container'>
+                    <span className='black-text left' id='welcomeMessage' >Welcome, <span id='userName'>{user.name}</span>!</span>
+                    <Link to='/search' className='black-text waves-effect waves-light navLink'>Search Harleys</Link>
+                    <Link to='/wishlists' className='black-text waves-effect waves-light navLink'>Your Wishlists</Link>
+                    <Link to='/about' className='black-text waves-effect waves-light navLink'>About</Link>
+                    &nbsp;&nbsp;<Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light right' id='logoutLink'>Log Out</Link>
+                </div>
+            </nav>
+        </div>
     )
 }
