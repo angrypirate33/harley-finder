0d557c048d1d85b7284c623c1d4743bdeab0f9b4
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-02 18:13:12 -0400

    Fixes issue with mobile nav.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,32 +10,34 @@ export default function Nav({ user, setUser }) {
     }
 
     useEffect(() => {
-        let elems = document.querySelectorAll('.sidenav');
-        let instances = window.M.Sidenav.init(elems, {});
-    }, []);
+        let elems = document.querySelectorAll('.sidenav')
+        let instances = window.M.Sidenav.init(elems, {})
+    }, [])
 
     return (
-        <div className='navbar-fixed'>
-            <nav className='orange'>
-                <div className='nav-wrapper container'>
-                    <span className='black-text left' id='welcomeMessage' >Welcome, <span id='userName'>{user.name}</span>!</span>
-                    <a href="#" data-target="mobile-links" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
-                    <ul className='right hide-on-med-and-down nav-links'>
-                        <li><Link to='/' className='black-text waves-effect waves-light'>Home</Link></li>
-                        <li><Link to='/search' className='black-text waves-effect waves-light'>Search Harleys</Link></li>
-                        <li><Link to='/wishlists' className='black-text waves-effect waves-light'>Your Wishlists</Link></li>
-                        <li><Link to='/about' className='black-text waves-effect waves-light'>About</Link></li>
-                        <li><Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light'>Log Out</Link></li>
-                    </ul>
-                </div>
-            </nav>
-            <ul className="sidenav" id="mobile-links">
-                <li><Link to='/' className='black-text waves-effect waves-light'>Home</Link></li>
-                <li><Link to='/search' className='black-text waves-effect waves-light'>Search Harleys</Link></li>
-                <li><Link to='/wishlists' className='black-text waves-effect waves-light'>Your Wishlists</Link></li>
-                <li><Link to='/about' className='black-text waves-effect waves-light'>About</Link></li>
-                <li><Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light'>Log Out</Link></li>
-            </ul>
-        </div>
+        <header>
+            <div className='navbar-fixed'>
+                <nav className='orange'>
+                    <div className='nav-wrapper container'>
+                        <a href="#" data-target="mobile-links" className="sidenav-trigger left"><i className="material-icons">menu</i></a>
+                        <span className='black-text left' id='welcomeMessage' >Welcome, <span id='userName'>{user.name}</span>!</span>
+                        <ul className='right hide-on-med-and-down nav-links'>
+                            <li><Link to='/' className='black-text waves-effect waves-light'>Home</Link></li>
+                            <li><Link to='/search' className='black-text waves-effect waves-light'>Search Harleys</Link></li>
+                            <li><Link to='/wishlists' className='black-text waves-effect waves-light'>Your Wishlists</Link></li>
+                            <li><Link to='/about' className='black-text waves-effect waves-light'>About</Link></li>
+                            <li><Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light'>Log Out</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+                <ul className="sidenav" id="mobile-links">
+                    <li><Link to='/' className='black-text waves-effect waves-light'>Home</Link></li>
+                    <li><Link to='/search' className='black-text waves-effect waves-light'>Search Harleys</Link></li>
+                    <li><Link to='/wishlists' className='black-text waves-effect waves-light'>Your Wishlists</Link></li>
+                    <li><Link to='/about' className='black-text waves-effect waves-light'>About</Link></li>
+                    <li><Link to="" onClick={handleLogOut} className='black-text waves-effect waves-light'>Log Out</Link></li>
+                </ul>
+        </header>
     )
 }
