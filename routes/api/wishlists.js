d4247baa6584403754537c79c51dbda267740a7c
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-05 09:28:53 -0400

    Adds ensureLoggedin function to CUD actions to meet project requirements.

--- routes/api/wishlists.js
+++ routes/api/wishlists.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const wishlistsCtrl = require('../../controllers/api/wishlists')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 router.get('/', wishlistsCtrl.getAll)
 router.get('/:id', wishlistsCtrl.getOne)
-router.post('/', wishlistsCtrl.create)
-router.put('/:id', wishlistsCtrl.update)
-router.post('/:id/motorcycles', wishlistsCtrl.addMotorcycle)
-router.delete('/:id/motorcycles', wishlistsCtrl.removeMotorcycle)
-router.delete('/:id', wishlistsCtrl.delete)
+router.post('/', ensureLoggedIn, wishlistsCtrl.create)
+router.put('/:id', ensureLoggedIn, wishlistsCtrl.update)
+router.post('/:id/motorcycles', ensureLoggedIn, wishlistsCtrl.addMotorcycle)
+router.delete('/:id/motorcycles', ensureLoggedIn, wishlistsCtrl.removeMotorcycle)
+router.delete('/:id', ensureLoggedIn, wishlistsCtrl.delete)
 
 module.exports = router
